277856dc69db898110d0b3d5359b9abb2918b00a
Severuna <oxxxyfomenkova@gmail.com>
2023-02-03 16:41:38 +0200

    create func for show images with interval & style edits

--- script/index.js
+++ script/index.js
@@ -1,6 +1,12 @@
 const backImageArr = [
-    '/images/banner_01.png',
-    '/images/banner_02.png',
-    '/images/banner_03.png',
-    '/images/banner_04.png'
-]
+    './images/banner_01.png',
+    './images/banner_02.png',
+    './images/banner_03.png',
+];
+const container = document.querySelector('.container');
+const showBackImage = () => {
+    let i = Math.floor(Math.random()*backImageArr.length);
+    container.style.backgroundImage = `url('${backImageArr[i]}')`;
+    setTimeout(showBackImage,2500);
+}
+window.onload = showBackImage;
